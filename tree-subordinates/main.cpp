4afacd1acc45753e8e83b4e7ced4e7df0530db42
Zigao Wang <zigao.wang24@gmail.com>
2024-12-30 14:42:51 +0800

    update

---
+++ tree-subordinates/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> adjList[200500];
+int ans[200500];
+int tree(int node, int parent) {
+    int sub = 0;
+    for (int na : adjList[node]) {
+        if (na == parent) continue;
+        sub += tree(na, node);
+    }
+    ans[node] = sub;
+    return sub + 1;
+}
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 2; i <= n; i++) {
+        int a;
+        cin >> a;
+        adjList[a].push_back(i);
+    }
+    tree(1, 0);
+    for (int i = 1; i <= n; i++) {
+        cout << ans[i] << " ";
+    }
+    return 0;
+}
