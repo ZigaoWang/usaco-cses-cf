e6581101ca5914ef53ff23653663ee458c7d743d
Zigao Wang <zigao.wang24@gmail.com>
2024-12-31 12:00:10 +0800

    update

---
+++ cses/1068-weird-algorithm/main.cpp
@@ -0,0 +1,29 @@
+// https://cses.fi/problemset/task/1068/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+
+    vector<long long> sequence;
+    sequence.push_back(n);
+
+    while (n != 1) {
+        if (n % 2 == 0) {
+            n /= 2;
+        } else {
+            n = 3 * n + 1;
+        }
+        sequence.push_back(n);
+    }
+
+    for (size_t i = 0; i < sequence.size() - 1; i++) {
+        cout << sequence[i] << " ";
+    }
+    cout << "1";
+
+    return 0;
+}
